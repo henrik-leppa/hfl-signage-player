bb085cbcbb014cd07bd8d3debc9d15a2b4c10292
Henrik Franciscus Leppä <35260600+henrik-leppa@users.noreply.github.com>
2018-02-25 14:12:34 +0200

    Add file upload to Management UI

--- back-end/app.js
+++ back-end/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 var express = require('express');
 var cors = require('cors');
 var multer = require('multer');
+var serveIndex = require('serve-index');
 var PouchDB = require('pouchdb');
 var expressPouchDB = require('express-pouchdb');
 
@@ -16,8 +17,14 @@ app.use('/' + databasePath, expressPouchDB(PouchDB));
 app.get('/', function(req, res){
   res.send(
     '<h1>React Digital Signage Player Back-end</h1>'
-    + '<p>Uploaded files can be found at: <code>/' + uploadPath + '</code></p>'
-    + '<p>PouchDB can be found at: <code>/' + databasePath + '</code></p>'
+    + '<p>'
+    + '  Uploaded files can be found at: '
+    + '  <a href="/' + uploadPath + '"><code>/' + uploadPath + '</code></a>'
+    + '</p>'
+    + '<p>'
+    + '  PouchDB can be found at: '
+    + '  <a href="/' + databasePath + '"><code>/' + databasePath + '</code></a>'
+    + '</p>'
   );
 });
 
@@ -34,8 +41,8 @@ app.post('/' + uploadPath, upload.single('file'), function (req, res, next) {
   console.log(req.body);
   return res.status(204).end();
 });
-
 app.use('/' + uploadPath, express.static(uploadPath));
+app.use('/' + uploadPath, serveIndex(__dirname + '/' + uploadPath));
 
 var port = process.env.PORT;
 
